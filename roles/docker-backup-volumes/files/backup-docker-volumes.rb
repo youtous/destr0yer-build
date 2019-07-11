489414abf6424b6ef89ead8184047668d136baec
youtous <contact@youtous.me>
2019-07-11 15:05:33 +0200

    pause containers before backup

--- roles/docker-backup-volumes/files/backup-docker-volumes.rb
+++ roles/docker-backup-volumes/files/backup-docker-volumes.rb
@@ -12,10 +12,25 @@ puts "[#{Time.now}] Saving docker volumes to #{save_path}"
 File.readlines(__dir__ + '/docker-volumes.txt').each do |docker_volume|
   docker_volume = docker_volume.strip
   puts "[#{Time.now}] Saving docker volume: #{docker_volume}"
+
+  # stop all other containers using this volume
+  containers_ids = `docker ps -aq --filter volume=#{docker_volume}`.split("\n")
+  puts "[#{Time.now}] Containers #{containers_ids} have to be paused. Processing..."
+  containers_ids.each do |id|
+    command = `docker pause #{id}`
+    puts "[#{Time.now}] #{command}"
+  end
+
   command = "docker run -v #{docker_volume}:/volume --rm loomchild/volume-backup backup - > #{save_path}/#{docker_volume}.taz.bz2"
   puts "[#{Time.now}] Executing backup... \n#{command}"
   output = `#{command}`
   puts "[#{Time.now}] #{output}"
+
+  puts "[#{Time.now}] Unpause paused containers."
+  containers_ids.each do |id|
+    command = `docker unpause #{id}`
+    puts "[#{Time.now}] #{command}"
+  end
 end
 
 puts "[#{Time.now}] All docker volumes has been successfully saved!"
