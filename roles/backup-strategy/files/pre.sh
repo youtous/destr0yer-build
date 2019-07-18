770636a0b26da6f5abd4a35dc207baf6a39f16df
youtous <contact@youtous.me>
2019-07-18 20:54:59 +0200

    duply backup script are loaded from a folder

--- roles/backup-strategy/files/pre.sh
+++ roles/backup-strategy/files/pre.sh
@@ -1,1 +1,11 @@
 #!/usr/bin/env bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+function include() {
+    for FILE in $( find "$1" -type f -print | sort )
+    do
+        source ${FILE}
+    done
+}
+
+# include pre scripts
+include ${DIR}/pre.d/*.sh
