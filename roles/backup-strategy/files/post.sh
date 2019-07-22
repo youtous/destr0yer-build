76af32e2ef1017624451cb2fdb641d72af012e6a
youtous <contact@youtous.me>
2019-07-22 19:52:22 +0200

    fix backup scripts

--- roles/backup-strategy/files/post.sh
+++ roles/backup-strategy/files/post.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 function include() {
-    for FILE in $( find "$1" -type f -print | sort )
+    for FILE in `ls $1 | sort`
     do
         source ${FILE}
     done
 }
 
 # include post scripts
-include ${DIR}/post.d/*.sh
+include "${DIR}/post.d/*.sh"
