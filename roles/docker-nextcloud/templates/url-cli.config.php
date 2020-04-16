cf52a2aee61f8502c1ec02894cac9b560ba2552e
youtous <contact@youtous.me>
2020-04-17 01:29:35 +0200

    using upstream version nextcloud

---
+++ roles/docker-nextcloud/templates/url-cli.config.php
@@ -0,0 +1,3 @@
+<?php
+// Use this configuration parameter to specify the base URL for any URLs which are generated within Nextcloud using any kind of command line tools (cron or occ). The value should contain the full base URL: https://www.example.com/nextcloud
+$CONFIG['overwrite.cli.url'] = 'https://{{ nextcloud_domain }}';
