975094850286a445ee493e47323663998b24a39f
youtous <contact@youtous.me>
2020-10-24 13:28:20 +0200

    added pull resolver ip docker-mailserver

---
+++ roles/docker-mailserver/files/scripts/update-resolver-ip.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+# This script is a workaround for docker-swarm fixed ip.
+# Due to the lack the fixed-ip service/container feature, the mailserver must resolve resolver service ip and update its
+# /etc/resolv.conf
+
+echo "Starting resolver service updater"
+
+# install dig if missing
+apt update && apt -y install --no-install-recommends dnsutils || exit 1
+
+while true; do
+  resolver_ip=`dig +short resolver`
+
+  if [ -n "${resolver_ip}" ]; then
+    # gather previous ip in resolv.conf
+    starting_ip=`echo "${resolver_ip}" | awk -F. {'print $1'}`
+    previous_ip=`grep -Po "^(?:nameserver\\s+)${starting_ip}\\.(.*)" /etc/resolv.conf | awk {'print $2'}`
+
+    # if ip !=
+    if [ "$previous_ip" != "$resolver_ip" ]; then
+      echo "Resolver service ip changed (from: ${previous_ip}, to: ${resolver_ip}), updating..."
+      cp /etc/resolv.conf /etc/resolv.conf.new
+      if [ -n "${previous_ip}" ]; then
+        # delete previous entry
+        sed -i "/nameserver ${previous_ip}/d" /etc/resolv.conf.new
+      fi
+      # prepend new nameserver
+      sed -i "1s;^;nameserver ${resolver_ip}\n;" /etc/resolv.conf.new
+      cp /etc/resolv.conf.new /etc/resolv.conf
+
+      # restart postfix if running
+      if supervisorctl status postfix | grep -q 'RUNNING'; then
+        supervisorctl restart postfix
+      fi
+      # restart dovecot if running
+      if supervisorctl status dovecot | grep -q 'RUNNING'; then
+        supervisorctl restart dovecot
+      fi
+    fi
+  fi
+
+   # check changes every 10 secs
+   sleep 10
+done
+
+
+exit 0
